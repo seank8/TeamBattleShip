84b186b144f14fa5b77c4b413bb638b975f2c638
Sean Khan <seankhanphotos@gmail.com>
2022-10-12 13:39:49 -0600

    Target Grid Super

--- src/TargetGrid.java
+++ src/TargetGrid.java
@@ -1,45 +1,23 @@
-public class TargetGrid {
-    private Cell[][] cells = new Cell[10][10];
 
-    public TargetGrid() {
-        for (int row = 0; row < 10; row++) {
-            for (int column = 0; column < 10; column++) {
-                cells [row][column] = new Cell();
+public class TargetGrid extends Grid{
+    
 
-            }
-
-        }
+    public TargetGrid(){
+        super();
     }
 
-    public void printGrid() {
-        printDivider();
-        printHeader();
-        printDivider();
 
-        for (int row = 0; row < 10; row++){
-            System.out.print("| A |");
-            for (int column = 0; column < 10; column++) {
-                System.out.print("   |");
-            }
-            System.out.println();
-            printDivider();
-        }
-            
-        
+    @Override
+    public void printGrid(){
 
-        
+        System.out.println("Target Grid:");
+        super.printGrid();
 
     }
+  
 
-    private void printHeader() {
-        
-        System.out.println("|   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |");
+    
+    
+    
 
-    }
-
-    private void printDivider(){
-        System.out.println("+---+---+---+---+---+---+---+---+---+---+---+");
-        
-    }
-
-} 
+}
