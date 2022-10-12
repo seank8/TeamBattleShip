8413a7ffab2ffc242eb992d94c797c9ceb99027f
Sean Khan <seankhanphotos@gmail.com>
2022-10-12 13:41:51 -0600

    Sean Coordinate and OceanGrid

---
+++ src/Coordinate.java
@@ -0,0 +1,18 @@
+public class Coordinate {
+    private int row;
+    private int column;
+
+    public Coordinate(int row, int column){
+        this.column = column;
+        this.row = row;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getColumn(){
+        return column;
+    }
+    
+}
