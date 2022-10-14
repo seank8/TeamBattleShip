84d2c79a215223d2caa4ddc80daf5a148d02e10c
justinal <justina.lwando@msubillings.edu>
2022-10-14 13:42:31 -0600

    removed extra methods and cleaned up code

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,9 @@
+import java.util.List;
 public class Player {
     private String name;
+
+    protected OceanGrid oceangrid = new OceanGrid();
+    protected TargetGrid targetGrid = new TargetGrid();
 
 
     public Player(String name){
@@ -10,6 +14,24 @@ public class Player {
         return name;
     }
 
+   
+ //row and column Position  bool, random do and while return
     
-}
+    public Shot takeShot(){
+        //present target grid and ocean grid
+        // get shot cordinates from user
+        //create shot object
+        //return shot object
+    }
+        
+
+    
+
+
+    
+
+    
+    
+       
+
 
