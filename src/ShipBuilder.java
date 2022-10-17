248a718803b01ff6fedf9dce31c4c325c450ab7d
Sean Khan <seankhanphotos@gmail.com>
2022-10-17 13:47:27 -0600

    Sean shipbuilder changes

--- src/ShipBuilder.java
+++ src/ShipBuilder.java
@@ -6,10 +6,18 @@ public class ShipBuilder{
     private OceanGrid ocean;
     private Random rand = new Random();
     private Coordinate origin;
+   
 
     public ShipBuilder(OceanGrid ocean){
         //repeat making of ship 5 times(1 for each ship)
         for (ShipType name : ShipType.values()){
+            ShipOrientation orientation = ShipOrientation.randomOrientation();
+            int length = name.getLength();
+            if (orientation == ShipOrientation.HORIZONTAL){
+                origin = new Coordinate(rand.nextInt(10), rand.nextInt(11 - length));
+            } else{
+                origin = new Coordinate(rand.nextInt(11 - length), rand.nextInt(10));
+            }
 
             
             ArrayList<Coordinate> coordinates = addCoords(name.getLength(), ShipOrientation.randomOrientation());
@@ -22,11 +30,7 @@ public class ShipBuilder{
 
     public ArrayList<Coordinate> addCoords(int length, ShipOrientation orientation){
         ArrayList<Coordinate> shipCoords = new ArrayList<Coordinate>();
-        if (orientation == ShipOrientation.HORIZONTAL){
-            origin = new Coordinate(rand.nextInt(10), rand.nextInt(11 - length));
-        } else{
-            origin = new Coordinate(rand.nextInt(11 - length), rand.nextInt(10));
-        }
+        
         for (int i = 1; i < length; i++){
 
         }
