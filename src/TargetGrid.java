eb4451d943f613b46083d394bdcf04cafbba588a
justinal <justina.lwando@msubillings.edu>
2022-10-19 14:12:44 -0600

    Justina added the targetgrid

--- src/TargetGrid.java
+++ src/TargetGrid.java
@@ -15,11 +15,33 @@ public class TargetGrid extends Grid{
 
     }
 
-    // return true or false
-    public boolean shotnottakenyet(Shot shot){
-        if();
+    // return true or false if the cell state is hit or miss 
+    public boolean isShotValid(Shot shot){
+        int r = shot.getRow();
+        int c = shot.getColumn();
 
+        //use to find cell at coordinate
+        Cell cell = cells[r][c];
+
+        //get state of cell, empyty or occupied
+        CellState cs = cell.getState();
+
+        if(cs == CellState.HIT){
+            return false;
+        }
+        
+        if (cs == CellState.MISS){
+            return false;
+        }
+        return true;
     }
+        
+}              
+         
+    
+
+
+    
     
   
 
@@ -27,4 +49,3 @@ public class TargetGrid extends Grid{
     
     
 
-}
