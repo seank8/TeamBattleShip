3fe41ab571fa830ac19dbfddbbaa39404c1bd63d
Sean Khan <seankhanphotos@gmail.com>
2022-10-21 14:35:09 -0600

    Target grid final hopefully

--- src/TargetGrid.java
+++ src/TargetGrid.java
@@ -9,8 +9,60 @@ public class TargetGrid extends Grid {
     public void printGrid() {
 
         System.out.println("Target Grid:");
-        
+        String rowName = "";
+        printDivider();
+        printHeader();
+        printDivider();
+        for(int row = 0; row < 10 ; row++){
+            
+            switch(row){
+                case 0 :
+                rowName = "| A |";
+                break;
+                case 1 :
+                rowName = "| B |";
+                break;
+                case 2 :
+                rowName = "| C |";
+                break;
+                case 3 :
+                rowName = "| D |";
+                break;
+                case 4 :
+                rowName = "| E |";
+                break;
+                case 5 :
+                rowName = "| F |";
+                break;
+                case 6 :
+                rowName = "| G |";
+                break;
+                case 7 :
+                rowName = "| H |";
+                break;
+                case 8 :
+                rowName = "| I |";
+                break;
+                case 9 :
+                rowName = "| J |";
+                break;
 
+            }
+            System.out.print(rowName);
+            for(int column = 0; column < 10; column++){
+                
+                if(cells[row][column].getState() == CellState.HIT){
+                    System.out.print(" X |");
+                }else if(cells[row][column].getState() == CellState.MISS){
+                    System.out.print(" O |");
+                }else{
+                    System.out.print("   |");
+                }
+                
+            }
+            System.out.println();
+            printDivider();
+        }
     }
 
     // return true or false if the cell state is hit or miss 
