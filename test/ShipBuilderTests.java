dabd3e9a68f595ec44e2922c77d6f8d348c9ccab
JohnP <john.pannell@msubillings.edu>
2022-10-21 14:47:01 -0600

    Added ShipBuilderTests class and made sample implementation of test.

---
+++ test/ShipBuilderTests.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ShipBuilderTests {
+    
+    @Test
+    public void testMakeCoordinates(){
+        // make a shipbuilder
+        // make a coordinate, orientation, and length for ship
+
+        // variable = shipbuilder.makeCoordinates(coord, orient, length)
+        
+        // make expected coordinates
+        // verify that method coordinates match expected
+    }
+}
