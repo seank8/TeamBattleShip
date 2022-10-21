31120abc0998065d4b87052d5f48338f183de3a2
MadsionCarr <mattiecarr98@gmail.com>
2022-10-21 11:18:06 -0600

    Adding Testing

---
+++ test/boardTest.java
@@ -0,0 +1,19 @@
+
+import org.junit.Test;
+
+
+public class boardTest {
+    @Test
+    public void TestBoardPrint()
+    {
+        new TargetGrid();
+
+    }
+    
+    @Test
+    public void TestOceanGridPrint() {
+        new OceanGrid();
+    }
+
+    
+}
