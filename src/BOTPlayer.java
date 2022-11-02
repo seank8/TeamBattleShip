a4c6dd76ece0e131095d9859ad9d1257feb69485
Sean Khan <seankhanphotos@gmail.com>
2022-11-02 13:53:27 -0600

    AI Player outline

---
+++ src/BOTPlayer.java
@@ -0,0 +1,13 @@
+public class BOTPlayer {
+    private String name;
+    private Shot shot;
+    protected OceanGrid oceanGrid = new OceanGrid();
+    protected TargetGrid targetGrid = new TargetGrid();
+    
+
+
+
+
+
+    
+}
