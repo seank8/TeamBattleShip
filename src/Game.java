e0148968880a717d4a099e79c319c436d6a0e6fe
MadsionCarr <mattiecarr98@gmail.com>
2022-11-14 21:36:37 -0700

    added AI options to launchScreen in Game class

--- src/Game.java
+++ src/Game.java
@@ -4,20 +4,19 @@ public class Game {
     private ArrayList<IPlayer> players = new ArrayList<IPlayer>();
     private int currentPlayerIndex = 0;
     private IPlayer winner;
-    
+
     // note from John - you don't need the player1 and player2 lines... your players are being kept in the players collection
     // you DO need an instance variable to keep track of which index is the current player (maybe currentPlayerIndex?)
 
+    public void play() {
 
-    public void play(){
-        
         launchScreen();
         players.forEach((player) -> player.placeShips());
 
-        while(true){ // game loop
+        while (true) { // game loop
 
             //start turn
-            
+
             IPlayer currentP = players.get(currentPlayerIndex);
 
             //send shot
@@ -26,56 +25,49 @@ public class Game {
             System.out.println(currentP.getName() + " Take a Shot!");
             Shot shot = currentP.takeshot();
 
-
             //return response
 
             IPlayer otherP;
-            if(currentPlayerIndex == 0){
+            if (currentPlayerIndex == 0) {
                 otherP = players.get(1);
-            }else{
+            } else {
                 otherP = players.get(0);
             }
             ShotResult result = otherP.receiveShot(shot);
 
             currentP.receiveShotResult(shot, result);
 
-
             //Print updated target grid and ocean grid
             currentP.printTargetGrid();
             currentP.printOceanGrid();
-            if(result == ShotResult.SUNK){
+            if (result == ShotResult.SUNK) {
                 System.out.println("YOU SUNK THEIR " + otherP.getRecentSunkShip() + "!!");
-            }else{
+            } else {
                 System.out.println("YOU GOT A " + result);
             }
-            
+
             //ask for player to press enter to go to next turn
             ConsoleHelper.getInput(currentP.getName() + " Press <enter> to go to next players turn: \n");
 
-
             //clear terminal 
             System.out.print("\033\143");
-            System.out.flush();  
+            System.out.flush();
 
             //check ships
-            if(otherP.allShipsAreSunk() == true){
+            if (otherP.allShipsAreSunk() == true) {
                 winner = currentP;
                 break;
             }
-            if(currentPlayerIndex == 0){
+            if (currentPlayerIndex == 0) {
                 ++currentPlayerIndex;
-            }else{
+            } else {
                 --currentPlayerIndex;
             }
-
-            
 
         }
         endGame(winner);
 
-        
-        
-    } 
+    }
 
     //consuole input hit enter before printing the turn
     //code that pause the game for entry before game
@@ -96,25 +88,48 @@ public class Game {
                 if (entry2.equals("2")) {
                     String entry3 = AIDifficultyMenu();
                     if (entry3.equals("1")) {
+                        System.out.println("You have choosen the Easy AI.");
                         String name1 = ConsoleHelper.getInput("Please enter name for Player: ");
                         players.add(new Player(name1));
 
                         String name2 = "JohnBot Infinity v2.3";
                         players.add(new BOTPlayer(name2)); 
+                    } else {
+                        if (entry3.equals("2")) {
+                            System.out.println("You have choosen the Medium AI.");
+                            String name1 = ConsoleHelper.getInput("Please enter name for Player: ");
+                            players.add(new Player(name1));
+
+                            String name2 = "JohnBot Infinity v2.3";
+                    //update with medium AI 
+                            players.add(new BOTPlayer(name2));
+
+                        }
+
+                        else {
+                            if (entry3.equals("3")) {
+                                System.out.println("You have choosen the Hard AI.");
+                                String name1 = ConsoleHelper.getInput("Please enter name for Player: ");
+                                players.add(new Player(name1));
+
+                                String name2 = "JohnBot Infinity v2.3";
+                    //replace with hard AI 
+                                players.add(new BOTPlayer(name2));
+
+                            }
+                        }
                     }
                     
                 }
                  
             }
-        }else{
-
         }
-
-        
     }
 
+        
+    
 
-    public String startupMenu(){
+    public String startupMenu() {
         System.out.println("Welcome to BattleShip!");
         System.out.println("Press '1' to Start a New Game");
         System.out.println("Press '2' to Load a Saved Game");
@@ -130,9 +145,9 @@ public class Game {
         String entry = ConsoleHelper.getInput("> ");
         return entry;
     }
-    
+
     public String AIDifficultyMenu() {
-        System.out.println("Choose a difficulty level to play against!");
+        System.out.println("Please choose a difficulty level!");
         System.out.println("Easy: Press '1'");
         System.out.println("Medium : Press '2'");
         System.out.println("Hard : Press '3'");
@@ -140,12 +155,9 @@ public class Game {
         return entry;
     }
 
-
-    public void endGame(IPlayer winner){
+    public void endGame(IPlayer winner) {
         System.out.println(winner.getName() + " IS THE WINNER!! ");
     }
 
-
-    
-    
 }
+
