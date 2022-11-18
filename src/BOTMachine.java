84870d190a41d52ca7aee99eada56426d6057407
Sean Khan <seankhanphotos@gmail.com>
2022-11-18 14:52:23 -0700

    Halfway Done

--- src/BOTMachine.java
+++ src/BOTMachine.java
@@ -1,5 +1,48 @@
 public class BOTMachine {
 
-    
+    public State nextState(State s, Event e){
+        State change = s;
+        switch(s){
+            case HUNT: switch(e){
+                case HIT:{
+                    change = State.BRACKET;
+                }    
+                case MISS:{
+                    change = State.HUNT;
+                }
+                case SUNK:{
+                    change = State.HUNT;
+                }
+                case OUT:{
+                    change = State.HUNT;
+                }
+            }
+            case BRACKET: switch(e){
+                case HIT:{
+                    change = State.PURSUIT;
+                }    
+                case MISS:{
+                    change = State.BRACKET;
+                }
+                case SUNK:{
+                    change = State.HUNT;
+                }
+                case OUT:{
+                    change = State.HUNT;
+                }
+            }
+            case PURSUIT:
+                break;
+            case REVERSE:
+                break;
+            default:
+                break;
+            
+            
+        }
+        return change;
+
+    }
+
     
 }
